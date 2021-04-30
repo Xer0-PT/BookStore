1389a76d9169915f16d487f03893d45f98b5ad36
Xer0-PT <joelflm@gmail.com>
2021-04-30 19:55:59 +0100

    DataContext

--- EGAPI/Models/Category.cs
+++ EGAPI/Models/Category.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace EGAPI.Models
 {
-    public class Categorie
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
