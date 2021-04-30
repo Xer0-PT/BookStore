cd78a4f78aebf99e5710222c61818ee08d0adbad
Xer0-PT <joelflm@gmail.com>
2021-04-30 18:45:37 +0100

    Initial commit

---
+++ EGAPI/Data/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EGAPI.Models;
+
+namespace EGAPI.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ EGAPI/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EGAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        List<Book> Books = new List<Book>();
+
+    }
+}
